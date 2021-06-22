dd61d3de48503f8f60f429fc073e15c72967eb28
m-hodges <michaelcshodges@gmail.com>
2021-06-23 10:58:55 +1200

    add base64 uploader to build

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -4,121 +4,119 @@
  */
 
 import {
-	Alignment,
-	AlignmentEditing,
-	AlignmentUI,
-} from '@ckeditor/ckeditor5-alignment';
+  Alignment,
+  AlignmentEditing,
+  AlignmentUI,
+} from "@ckeditor/ckeditor5-alignment";
 
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
+import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
 // The editor creator to use.
-import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Link from '@ckeditor/ckeditor5-link/src/link';
-import List from '@ckeditor/ckeditor5-list/src/list';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-import Table from '@ckeditor/ckeditor5-table/src/table';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading";
+import Image from "@ckeditor/ckeditor5-image/src/image";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import Link from "@ckeditor/ckeditor5-link/src/link";
+import List from "@ckeditor/ckeditor5-list/src/list";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import Table from "@ckeditor/ckeditor5-table/src/table";
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
+import UploadAdapter from "@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter";
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
-	Essentials,
-	UploadAdapter,
-	Autoformat,
-	Bold,
-	Italic,
-	BlockQuote,
-	CKFinder,
-	CloudServices,
-	EasyImage,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	ImageResize,
-	Indent,
-	Link,
-	List,
-	MediaEmbed,
-	Paragraph,
-	PasteFromOffice,
-	Table,
-	TextTransformation,
-	Alignment,
-	AlignmentEditing,
-	AlignmentUI,
+  Essentials,
+  UploadAdapter,
+  Autoformat,
+  Bold,
+  Italic,
+  BlockQuote,
+  CKFinder,
+  Heading,
+  Image,
+  ImageCaption,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  ImageResize,
+  Indent,
+  Link,
+  List,
+  Paragraph,
+  PasteFromOffice,
+  Table,
+  TextTransformation,
+  Alignment,
+  AlignmentEditing,
+  AlignmentUI,
+  Base64UploadAdapter,
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'alignment',
-			'outdent',
-			'indent',
-			'blockQuote',
-			'|',
-			'undo',
-			'redo',
-		],
-	},
-	image: {
-		styles: ['alignLeft', 'alignCenter', 'alignRight'],
-		resizeOptions: [
-			{
-				name: 'resizeImage:original',
-				label: 'Original',
-				value: null,
-			},
-			{
-				name: 'resizeImage:50',
-				label: '50%',
-				value: '50',
-			},
-			{
-				name: 'resizeImage:75',
-				label: '75%',
-				value: '75',
-			},
-		],
-		toolbar: [
-			'imageStyle:alignLeft',
-			'imageStyle:alignCenter',
-			'imageStyle:alignRight',
-			'|',
-			'resizeImage',
-			'|',
-			'imageTextAlternative',
-		],
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en',
+  toolbar: {
+    items: [
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "link",
+      "bulletedList",
+      "numberedList",
+      "|",
+      "alignment",
+      "outdent",
+      "indent",
+      "blockQuote",
+      "|",
+      "uploadImage",
+      "|",
+      "undo",
+      "redo",
+    ],
+  },
+  image: {
+    styles: ["alignLeft", "alignCenter", "alignRight"],
+    resizeOptions: [
+      {
+        name: "resizeImage:original",
+        label: "Original",
+        value: null,
+      },
+      {
+        name: "resizeImage:50",
+        label: "50%",
+        value: "50",
+      },
+      {
+        name: "resizeImage:75",
+        label: "75%",
+        value: "75",
+      },
+    ],
+    toolbar: [
+      "imageStyle:alignLeft",
+      "imageStyle:alignCenter",
+      "imageStyle:alignRight",
+      "|",
+      "resizeImage",
+      "|",
+      "imageTextAlternative",
+    ],
+  },
+  // This value must be kept in sync with the language defined in webpack.config.js.
+  language: "en",
 };
